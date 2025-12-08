8112631f994a1aadc808f6b869232b3fbf0716b6
GitHub1Adolfo <167332944+GitHub1Adolfo@users.noreply.github.com>
2025-12-08 00:37:01 -0300

    Delete NegocioModel directory

--- NegocioModel/tiendaApp/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from serialApp.models import Student
-
-class StudentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Student
-        fields = '__all__'  # Todos los campos del modelo
